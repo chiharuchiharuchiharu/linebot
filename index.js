6bec00a90670bcb3be0c19fea44374e6702fda51
chiharuchiharuchiharu <x23060xx@aitech.ac.jp>
2023-06-19 16:14:00 +0900

    リッチメニューテスト

--- index.js
+++ index.js
@@ -1,527 +1,77 @@
-"use strict";
-
-const express = require("express");
-const line = require("@line/bot-sdk");
-const PORT = process.env.PORT || 3000;
+// CommonJS
+const line = require('@line/bot-sdk');
+const { join } = require("path");
+const { readFileSync } = require("fs");
+const {richMenuObjectA, richMenuObjectB} = require('./rich_menu_object.js');
 
 const config = {
   channelSecret: process.env.CHANNEL_SECRET,
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
 };
 
-const app = express();
+client = new line.Client(config);
 
-app.get("/", (req, res) => res.send("Hello LINE BOT!(GET)")); //ブラウザ確認用(無くても問題ない)
-app.post("/webhook", line.middleware(config), (req, res) => {
-  Promise.all(req.body.events.map(handleEvent)).then((result) =>
-    res.json(result)
-  );
-});
-
-const client = new line.Client(config);
-
-async function handleEvent(event) {
-  if (event.type !== "message" || event.message.type !== "text") {
-    return Promise.resolve(null);
-  }
-  let repMessage = "";
-  const message = event.message.text;
-
-  switch (message) {
-    case "week": {
-      repMessage = {
-        type: "flex",
-        altText: "Flex Message",
-        contents: {
-          type: "carousel",
-          contents: [
-            {
-              type: "bubble",
-              size: "micro",
-              body: {
-                type: "box",
-                layout: "vertical",
-                contents: [
-                  {
-                    type: "text",
-                    text: "7/24(月)",
-                    size: "xl",
-                  },
-                  {
-                    type: "text",
-                    text: "〜7/30(日)",
-                    size: "xl",
-                  },
-                ],
-                height: "150px",
-                justifyContent: "center",
-                alignItems: "center",
-                action: {
-                  type: "message",
-                  label: "week",
-                  text: "7/24-7/30",
-                },
-              },
-              styles: {
-                footer: {
-                  separator: false,
-                },
-              },
-            },
-            {
-              type: "bubble",
-              size: "micro",
-              body: {
-                type: "box",
-                layout: "vertical",
-                contents: [
-                  {
-                    type: "text",
-                    text: "7/31(月)",
-                    size: "xl",
-                  },
-                  {
-                    type: "text",
-                    text: "〜8/6(日)",
-                    size: "xl",
-                  },
-                ],
-                height: "150px",
-                justifyContent: "center",
-                alignItems: "center",
-                action: {
-                  type: "message",
-                  label: "week",
-                  text: "7/31-8/6",
-                },
-              },
-              styles: {
-                footer: {
-                  separator: false,
-                },
-              },
-            },
-            {
-              type: "bubble",
-              size: "micro",
-              body: {
-                type: "box",
-                layout: "vertical",
-                contents: [
-                  {
-                    type: "text",
-                    text: "8/7(月)",
-                    size: "xl",
-                  },
-                  {
-                    type: "text",
-                    text: "〜8/13(日)",
-                    size: "xl",
-                  },
-                ],
-                height: "150px",
-                justifyContent: "center",
-                alignItems: "center",
-                action: {
-                  type: "message",
-                  label: "week",
-                  text: "8/7-8/13",
-                },
-              },
-              styles: {
-                footer: {
-                  separator: false,
-                },
-              },
-            },
-            {
-              type: "bubble",
-              size: "micro",
-              body: {
-                type: "box",
-                layout: "vertical",
-                contents: [
-                  {
-                    type: "text",
-                    text: "8/14(月)",
-                    size: "xl",
-                  },
-                  {
-                    type: "text",
-                    text: "〜8/20(日)",
-                    size: "xl",
-                  },
-                ],
-                height: "150px",
-                justifyContent: "center",
-                alignItems: "center",
-                action: {
-                  type: "message",
-                  label: "week",
-                  text: "8/14-8/20",
-                },
-              },
-              styles: {
-                footer: {
-                  separator: false,
-                },
-              },
-            },
-          ],
-        },
-      };
-      break;
+const reset = async() => {
+  // 全 alias を選択する
+  client.getRichMenuAliasList().then((res) => {
+    for(var richMenu of res.aliases) {
+      // alias を削除する
+      client.deleteRichMenuAlias(richMenu.richMenuAliasId)
     }
-    case "day": {
-      repMessage = {
-        type: "flex",
-        altText: "Flex Message",
-        contents: {
-          type: "carousel",
-          contents: [
-            {
-              type: "bubble",
-              size: "giga",
-              body: {
-                type: "box",
-                layout: "vertical",
-                contents: [
-                  {
-                    type: "box",
-                    layout: "horizontal",
-                    contents: [
-                      {
-                        type: "box",
-                        layout: "vertical",
-                        contents: [
-                          {
-                            type: "text",
-                            text: "7/24(月)",
-                            size: "xxl",
-                          },
-                        ],
-                        backgroundColor: "#FFD876",
-                        offsetEnd: "none",
-                        cornerRadius: "lg",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        width: "160px",
-                        action: {
-                          type: "postback",
-                          label: "day",
-                          data: "7-24",
-                          displayText: "7/24(月)",
-                        },
-                      },
-                      {
-                        type: "box",
-                        layout: "vertical",
-                        contents: [
-                          {
-                            type: "text",
-                            text: "7/25(火)",
-                            size: "xxl",
-                          },
-                        ],
-                        backgroundColor: "#FFD876",
-                        offsetStart: "none",
-                        cornerRadius: "lg",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        width: "160px",
-                        action: {
-                          type: "postback",
-                          label: "day",
-                          data: "7-25",
-                          displayText: "7/25(火)",
-                        },
-                      },
-                    ],
-                    height: "160px",
-                    justifyContent: "space-evenly",
-                  },
-                  {
-                    type: "box",
-                    layout: "horizontal",
-                    contents: [
-                      {
-                        type: "box",
-                        layout: "vertical",
-                        contents: [
-                          {
-                            type: "text",
-                            text: "7/26(水)",
-                            size: "xxl",
-                          },
-                        ],
-                        backgroundColor: "#FFD876",
-                        offsetEnd: "none",
-                        cornerRadius: "lg",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        width: "160px",
-                        action: {
-                          type: "postback",
-                          label: "day",
-                          data: "7-26",
-                          displayText: "7/26(水)",
-                        },
-                      },
-                      {
-                        type: "box",
-                        layout: "vertical",
-                        contents: [
-                          {
-                            type: "text",
-                            text: "7/27(木)",
-                            size: "xxl",
-                          },
-                        ],
-                        backgroundColor: "#FFD876",
-                        offsetStart: "none",
-                        cornerRadius: "lg",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        width: "160px",
-                        action: {
-                          type: "postback",
-                          label: "day",
-                          data: "7-27",
-                          displayText: "7/27(木)",
-                        },
-                      },
-                    ],
-                    height: "160px",
-                    justifyContent: "space-evenly",
-                  },
-                  {
-                    type: "box",
-                    layout: "horizontal",
-                    contents: [
-                      {
-                        type: "box",
-                        layout: "vertical",
-                        contents: [
-                          {
-                            type: "text",
-                            text: "7/28(金)",
-                            size: "xxl",
-                          },
-                        ],
-                        backgroundColor: "#FFD876",
-                        offsetEnd: "none",
-                        cornerRadius: "lg",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        width: "160px",
-                        action: {
-                          type: "postback",
-                          label: "day",
-                          data: "7-28",
-                          displayText: "7/28(金)",
-                        },
-                      },
-                      {
-                        type: "box",
-                        layout: "vertical",
-                        contents: [
-                          {
-                            type: "text",
-                            text: "7/29(土)",
-                            size: "xxl",
-                          },
-                        ],
-                        backgroundColor: "#FFD876",
-                        offsetStart: "none",
-                        cornerRadius: "lg",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        width: "160px",
-                        action: {
-                          type: "postback",
-                          label: "day",
-                          data: "7-29",
-                          displayText: "7/29(土)",
-                        },
-                      },
-                    ],
-                    height: "160px",
-                    justifyContent: "space-evenly",
-                  },
-                  {
-                    type: "box",
-                    layout: "horizontal",
-                    contents: [
-                      {
-                        type: "box",
-                        layout: "vertical",
-                        contents: [
-                          {
-                            type: "text",
-                            text: "7/30(日)",
-                            size: "xxl",
-                          },
-                        ],
-                        backgroundColor: "#FFD876",
-                        offsetEnd: "none",
-                        cornerRadius: "lg",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        width: "160px",
-                        action: {
-                          type: "postback",
-                          label: "day",
-                          data: "7-30",
-                          displayText: "7/30(日)",
-                        },
-                      },
-                      {
-                        type: "box",
-                        layout: "vertical",
-                        contents: [
-                          {
-                            type: "text",
-                            text: "キャンセル",
-                            size: "xxl",
-                          },
-                        ],
-                        backgroundColor: "#FFD876",
-                        offsetStart: "none",
-                        cornerRadius: "lg",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        width: "160px",
-                        action: {
-                          type: "postback",
-                          label: "day",
-                          data: "cancel",
-                          displayText: "キャンセル",
-                        },
-                      },
-                    ],
-                    height: "160px",
-                    justifyContent: "space-evenly",
-                  },
-                ],
-                spacing: "md",
-              },
-              styles: {
-                footer: {
-                  separator: false,
-                },
-              },
-            },
-          ],
-        },
-      };
+  })
 
-      break;
+  // 全リッチメニューを選択する
+  client.getRichMenuList().then((res) => {
+    for(var richMenu of res) {
+      // リッチメニューを削除する
+      client.deleteRichMenu(richMenu.richMenuId)
     }
-    case "register": {
-      repMessage = {
-        type: "flex",
-        altText: "Flex Message",
-        contents: {
-          type: "carousel",
-          contents: [
-            {
-              type: "bubble",
-              size: "giga",
-              body: {
-                type: "box",
-                layout: "vertical",
-                contents: [
-                  {
-                    type: "box",
-                    layout: "horizontal",
-                    contents: [
-                      {
-                        type: "box",
-                        layout: "vertical",
-                        contents: [
-                          {
-                            type: "text",
-                            text: "はい",
-                            size: "xl",
-                            action: {
-                              type: "postback",
-                              label: "register",
-                              data: "yes",
-                              displayText: "はい",
-                            },
-                          },
-                        ],
-                        backgroundColor: "#FFD876",
-                        offsetEnd: "none",
-                        cornerRadius: "lg",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        width: "160px",
-                        action: {
-                          type: "postback",
-                          label: "day",
-                          data: "7-24",
-                          displayText: "7/24(月)",
-                        },
-                      },
-                      {
-                        type: "box",
-                        layout: "vertical",
-                        contents: [
-                          {
-                            type: "text",
-                            text: "キャンセル",
-                            size: "xl",
-                            action: {
-                              type: "postback",
-                              label: "register",
-                              data: "cancel",
-                              displayText: "キャンセル",
-                            },
-                          },
-                        ],
-                        backgroundColor: "#FFD876",
-                        offsetStart: "none",
-                        cornerRadius: "lg",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        width: "160px",
-                        action: {
-                          type: "postback",
-                          label: "day",
-                          data: "7-25",
-                          displayText: "7/25(火)",
-                        },
-                      },
-                    ],
-                    height: "40px",
-                    justifyContent: "space-evenly",
-                  },
-                ],
-                spacing: "md",
-              },
-              styles: {
-                footer: {
-                  separator: false,
-                },
-              },
-            },
-          ],
-        },
-      };
-
-      break;
-    }
-
-    default: {
-      repMessage = {
-        type: "text",
-        text: message,
-      };
-    }
-  }
-  return client.replyMessage(event.replyToken, repMessage);
+  })
 }
 
-app.listen(PORT);
-console.log(`Server running at ${PORT}`);
+// rich_menu_object でリッチメニューの構成を指定する
+// リッチメニューオブジェクト: https://developers.line.biz/ja/reference/messaging-api/#rich-menu-object
+const createRichMenu = async (richMenuObject) => {
+  return await client.createRichMenu(
+    richMenuObject
+  )
+}
+
+// リッチメニューに画像をアップロードして添付する
+const setRichMenuImage = async (richMenuId, path) => {
+  const filepath = join(__dirname, path);
+  const buffer = readFileSync(filepath);
+
+  const res = await client.setRichMenuImage(richMenuId, buffer);
+}
+
+// デフォルトのリッチメニューを設定する
+const setDefaultRichMenu = async (richMenuId) => {
+  return await client.setDefaultRichMenu(richMenuId)
+}
+
+// リッチメニューの alias の登録
+const setRichMenuAlias = async (richMenuId, richMenuAliasId) => {
+  return await client.createRichMenuAlias(richMenuId, richMenuAliasId)
+}
+
+const main = async () => {
+  // 2. リッチメニューA（richmenu-a）を作成する
+  const richMenuAId = await createRichMenu(richMenuObjectA())
+  // 3. リッチメニューAに画像をアップロードする
+  await setRichMenuImage(richMenuAId, '../public/richmenu-a.png')
+  // 4. リッチメニューB（richmenu-b）を作成する
+  const richMenuBId = await createRichMenu(richMenuObjectB())
+  // 5. リッチメニューBに画像をアップロードする
+  await setRichMenuImage(richMenuBId, '../public/richmenu-b.png')
+  // 6. リッチメニューAをデフォルトのリッチメニューにする
+  await setDefaultRichMenu(richMenuAId)
+  // 7. リッチメニューエイリアスAを作成する
+  await setRichMenuAlias(richMenuAId, 'richmenu-alias-a')
+  // 8. リッチメニューエイリアスBを作成する
+  await setRichMenuAlias(richMenuBId, 'richmenu-alias-b')
+  console.log('success')
+}
+
+reset()
+main()
